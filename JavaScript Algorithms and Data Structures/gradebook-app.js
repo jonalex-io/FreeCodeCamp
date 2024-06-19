e4e7bee4cfe497e333e9593cb55af2a8f86cc462
jonalex <121149817+jonalex-io@users.noreply.github.com>
2024-06-19 02:26:42 -0600

    Update gradebook-app.js

--- JavaScript Algorithms and Data Structures/gradebook-app.js
+++ JavaScript Algorithms and Data Structures/gradebook-app.js
@@ -6,7 +6,6 @@ function getAverage(scores) {
     }
     return sum / scores.length
 }
-
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
 
@@ -26,7 +25,6 @@ function getGrade(score) {
         return "F"
     }
 }
-
 console.log(getGrade(96));
 console.log(getGrade(82));
 console.log(getGrade(56));
@@ -35,7 +33,16 @@ console.log(getGrade(56));
 function hasPassingGrade(score) {
     return getGrade(score) !== "F";
 }
-
 console.log(hasPassingGrade(100));
 console.log(hasPassingGrade(53));
 console.log(hasPassingGrade(87));
+
+// MESSAGE_STUDENT
+function studentMsg(totalScores, studentScore) {
+    if (getGrade(studentScore) == "F") {
+        return "Class average: " + getAverage(totalScores) + ". Your grade: " + getGrade(studentScore) + ". You failed the course."
+    } else {
+        return "Class average: " + getAverage(totalScores) + ". Your grade: " + getGrade(studentScore) + ". You passed the course."
+    }
+}
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
