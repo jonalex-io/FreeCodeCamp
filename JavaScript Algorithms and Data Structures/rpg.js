fbf7702d35f5b57138312bf36f44716d10a83ca1
jonalex <121149817+jonalex-io@users.noreply.github.com>
2024-06-22 14:23:48 -0600

    進步rpg.js

--- JavaScript Algorithms and Data Structures/rpg.js
+++ JavaScript Algorithms and Data Structures/rpg.js
@@ -5,17 +5,17 @@ let currentWeaponIndex = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
-const button1 = document.querySelector("#button1")
-const button2 = document.querySelector("#button2")
-const button3 = document.querySelector("#button3")
 
-const text = document.querySelector("#text")
-const xpText = document.querySelector("#xpText")
-const healthText = document.querySelector("#healthText")
-const goldText = document.querySelector("#goldText")
-const monsterStats = document.querySelector("#monsterStats")
-const monsterName = document.querySelector("#monsterName")
-const monsterHealthText = document.querySelector("#monsterHealth")
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
 
 // initialize buttons
 button1.onclick = goStore;
@@ -32,9 +32,9 @@ function goStore() {
 }
 
 function goCave() {
-    console.log("Going to cave.")
+    console.log("Going to cave.");
 }
 
 function fightDragon() {
-    console.log("Fighting dragon.")
+    console.log("Fighting dragon.");
 }
