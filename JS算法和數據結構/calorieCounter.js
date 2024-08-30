4a92b93169dbc434ffef3bcced7713f48c5f9942
jonalex <121149817+jonalex-io@users.noreply.github.com>
2024-08-30 13:16:19 -0600

    開始運用控制台來實驗次程式

--- JS算法和數據結構/calorieCounter.js
+++ JS算法和數據結構/calorieCounter.js
@@ -1,21 +1,43 @@
-// 先定變數
+// ***************
+// *** 先定變數 ***
 const calorieCounter = document.getElementById('calorie-counter');
 const budgetNumberInput = document.getElementById('budget');
 const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
-// 標準除錯方法？
+// 標準除錯方法嗎
 let isError = false;
 
+// *************
+// *** 次程式 ***
+
+// Remove [+-\s] from user's numerical input
 function cleanInputString(str) {
+  // Regular Expression IS A PATTERN of characters
+  // Pattern is used for searching & replacing characters in strings
+  // /pattern/modifier(s);
+  // Modifiers include:
+  //    * /g - global match (find all)
+  //    * /i - case-insensitive matching
+  //    * /m - multi-line matching
+  // Pattern may use brackets:
+  //    * [abc] - find any character between the brackets
+  //    * [0-9] - find any digit between the brackets
+  // RegExp object has Properties & Methods:
+  //    * toString()
   const regex = /[+-\s]/g;
   return str.replace(regex, '');
 }
 
+// Check whether user is using scientific notation
 function isInvalidInput(str) {
   const regex = /\d+e\d+/i;
   return str.match(regex);
+  // match():
+  //    * matches a string against a regex
+  //    * returns an array with the matches
+  //    * returns null if no match found
 }
 
 function addEntry() {
@@ -101,3 +123,16 @@ function clearForm() {
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
 clearButton.addEventListener("click", clearForm);
+
+// ***********
+// *** 實驗 ***
+console.log("%c FUNCTION cleanInputString(str)", "font-weight: bold;");
+console.log(cleanInputString("+100"))
+console.log(cleanInputString("-100"))
+console.log(cleanInputString("1 0 0"))
+console.log(cleanInputString("= 1 0 0"))
+
+console.log("%c FUNCTION isInvalidInput(str)", "font-weight: bold;");
+console.log(isInvalidInput("10e1"));
+console.log(isInvalidInput("10E1"));
+console.log(isInvalidInput("101"));
